ac43ba261a76b52d26a9b0b867a8d54ddf8a6e14
Alex Thalhammer <alex@angular-software.at>
2021-10-13 09:23:13 +0200

    [Routing]

--- src/app/flight-booking/flight-booking.module.ts
+++ src/app/flight-booking/flight-booking.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 import { SharedModule } from '../shared/shared.module';
 import { FlightSearchComponent } from './flight-search/flight-search.component';
 import { FlightCardComponent } from './flight-card/flight-card.component';
 import { FlightStatusToggleComponent } from './flight-status-toggle/flight-status-toggle.component';
+import { FLIGHT_BOOKING_ROUTES } from './flight-booking.routes';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, SharedModule],
+  imports: [CommonModule, FormsModule, RouterModule.forChild(FLIGHT_BOOKING_ROUTES), SharedModule],
   declarations: [FlightSearchComponent, FlightCardComponent, FlightStatusToggleComponent],
   providers: [],
   exports: [FlightSearchComponent]
